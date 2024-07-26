9d80ee381937bef50bd682500ca6dca2d179ed1e
kjmjh <18369898058@163.com>
2024-07-26 10:20:55 +0800

    更新 ss.js

--- ss.js
+++ ss.js
@@ -1,28 +1,8 @@
-// 获取要操作的元素
-const items=document.querySelectorAll('.item');
-const sections=document.querySelectorAll('section');
+// 要操作的元素
+const menu_box=document.querySelector('.menu-box');
+const menu_button=document.querySelector('.menu-button');
 
-// 移除选中态
-function removeActive(){
-    // 移除标签选中态样式
-    items.forEach(item=>{
-        item.classList.remove('active');
-    });
-    // 移除内容区选中态样式
-    sections.forEach(item=>{
-        item.classList.remove('active');
-    });
-}
-
-// 遍历所有标签
-items.forEach((item,index)=>{
-    // 为每个标签绑定点击事件
-    item.addEventListener('click',function(){
-        // 移除选中态样式
-        removeActive();
-        // 为当前标签添加选中样式
-        item.classList.add('active');
-        // 为当前内容区添加选中样式
-        sections[index].classList.add('active');
-    })
+// 为菜单按钮绑定点击事件
+menu_button.addEventListener('click',function(){
+    menu_box.classList.toggle('active');
 })
